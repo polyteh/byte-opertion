f463cfde90925a0681cbf5b6fb160875d96230e0
polyteh <44604978+polyteh@users.noreply.github.com>
2019-01-22 12:52:27 +0200

    Add files via upload

---
+++ build/CMakeFiles/byte.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/byte.dir/byte.cpp.obj"
+  "byte.pdb"
+  "byte.exe"
+  "byte.exe.manifest"
+  "libbyte.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/byte.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
